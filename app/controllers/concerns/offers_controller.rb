c5683763a15b5fa820bbd74bfdebf25b1b692a72
Masahiro Nomura <masahironomura5325@gmail.com>
2015-12-27 18:06:22 +0900

    add women-top

---
+++ app/controllers/concerns/offers_controller.rb
@@ -0,0 +1,5 @@
+class OffersController < ActionController::Base
+  def index
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   match '/guest_top', to: 'test#guest_top', via: 'get'
 
+  match '/top_women', to: 'offers#index', via: 'get'
+
 end
